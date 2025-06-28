2275f319c62509064f9a5e77e8950933f36e0527
Lydi <apple@MacBookPro.lan>
2025-06-28 14:02:21 +0300

    calculation of the shipping cost

--- conditions.dart
+++ conditions.dart
@@ -4,20 +4,22 @@ void main() {
 
     String destination = "XYZ";
 
+    double weight = 6.9;
+
     if (destination == "XYZ"){
 
-        shippingCost = 5;
+        shippingCost = 5 * weight;
       
         print("The shipping cost is $shippingCost dollars.");
     }
     else if (destination == "ABC") {
 
-        shippingCost = 7;
+        shippingCost = 7 * weight;
         print("The shipping cost is $shippingCost dollars.");
     }
     else if (destination == "PQR") {
 
-        shippingCost = 10;
+        shippingCost = 10 * weight;
         print("The shipping cost is $shippingCost dollars.");
     }
 
