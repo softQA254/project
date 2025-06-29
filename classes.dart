ca46757bde8c1a2b764e8abb1b312da01a7c336c
Lydi <okothlyd@gmail.com>
2025-06-29 16:44:33 +0300

    define a class with attributes & methods

---
+++ classes.dart
@@ -0,0 +1,29 @@
+void main() {
+
+}
+class Salon {
+
+  //Attributes
+
+  Sting makeup;
+  String braids;
+  int plaitAmount;
+  int braidsCost;
+  double bookingTime;
+
+  //methods
+
+double starttime(){
+  return 08:00
+}
+
+bool isComplete() {
+  => true || false
+}
+
+String giveFeedback() {
+
+  return " "
+}
+
+}
