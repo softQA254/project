74e5e1eb7611f8aa3b78de9dc18bfc8c5dc4bf63
Lydi <okothlyd@gmail.com>
2025-06-29 12:40:34 +0300

    show functions syntax

---
+++ functions.dart
@@ -0,0 +1,14 @@
+/*
+function syntax:
+
+datatype, function name () {
+}
+
+void +> shows that the function does not return any value
+
+ */
+
+void printName () {
+
+    print("Lydia")
+} // this is a function that does not return any value but only displays the name Lydia.
